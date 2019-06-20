979a28c1cc79fefc860abc0ecae8b5621133f325
alexfiftysix <amiller5656@gmail.com>
2019-06-20 13:16:42 +1000

    Front end correctly displays attended/payed for bookings

--- student-tracker-frontend/src/components/DailyView.js
+++ student-tracker-frontend/src/components/DailyView.js
@@ -41,14 +41,12 @@ export default function DailyView(props) {
         return (<div className={'booking-list'}>{bookings['message']}</div>);
     }
 
-
-
     return (
         <div className={'booking-list'}>
             <h2><Link to={'/daily/' + date}>{day}</Link></h2>
             <h5>{date}</h5>
             {!bookings ? 'Loading...' : bookings.map(b =>
-                <Booking key={b.lesson_plan.id} name={b.name} time={b.lesson_plan.lesson_time} attended={'???'} payed={'???'}
+                <Booking key={b.lesson_plan.id} name={b.name} time={b.lesson_plan.lesson_time} attended={b.attended} payed={b.payed}
                          address={b.address} id={b.id} price={b.lesson_plan.price} end_time={b.lesson_plan.end_time}
                          student_id={b.id}/>
             )}
