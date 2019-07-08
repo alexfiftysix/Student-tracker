79df809f9d3187d5b6b7ebe07ef6674922a5ca79
alexfiftysix <amiller5656@gmail.com>
2019-07-08 22:40:17 +1000

    Simplfy/redisign of daily view

--- student-tracker-frontend/src/components/DailyView.js
+++ student-tracker-frontend/src/components/DailyView.js
@@ -1,9 +1,27 @@
 import React, {useEffect} from 'react'
 import {Link} from "react-router-dom";
 import Booking from "./Booking";
-import './DailyView.css'
+import Paper from '@material-ui/core/Paper'
+import {makeStyles} from "@material-ui/core";
+// import './DailyView.css'
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        margin: theme.spacing(0.2),
+        padding: theme.spacing(1),
+        backgroundColor: '#f5f5ef',
+    },
+    header: {
+        textDecoration: 'none',
+        color: 'black',
+        "&:visited": {
+            color: 'black',
+        }
+    }
+}));
 
 export default function DailyView(props) {
+    const classes = useStyles();
     const [bookings, setBookings] = React.useState(null);
     const date = props.date ? props.date : props.match.params.start_date;
 
@@ -42,13 +60,13 @@ export default function DailyView(props) {
 
 
     return (
-        <div className={'booking-list'}>
-            <h2><Link to={'/daily/' + date}>{day}</Link></h2>
+        <Paper className={classes.paper}>
+            <h2 ><Link to={'/daily/' + date} className={classes.header}>{day}</Link></h2>
             <h5>{date}</h5>
             {!bookings ? 'Loading...' : bookings.map(b =>
                 <Booking key={b.lesson_plan.id} attended={b.attended} payed={b.payed}
                          date={date} booking={b}/>
             )}
-        </div>
+        </Paper>
     );
 }
