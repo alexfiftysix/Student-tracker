62b9ce9c025ce9416ffc69d821bed3dadd138566
alexfiftysix <amiller5656@gmail.com>
2019-07-16 09:36:19 +1000

    Changed dayNavigator to use date-fns for date formatting

--- student-tracker-frontend/src/components/dayNavigator.js
+++ student-tracker-frontend/src/components/dayNavigator.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import {Link} from "react-router-dom";
+import {Link} from "react-router-dom"
 import DailyView from './DailyView'
 import './dayNavigator.css'
-import {makeStyles} from "@material-ui/core";
-import ChevronLeft from '@material-ui/icons/ChevronLeft'
-import ChevronRight from '@material-ui/icons/ChevronRight'
+import {makeStyles} from "@material-ui/core"
+import dateFns from "date-fns"
 
 const useStyles = makeStyles(theme => ({
     navigator: {
@@ -38,29 +37,30 @@ const useStyles = makeStyles(theme => ({
 
 export default function DayNavigator(props) {
     const classes = useStyles();
+    const dateFormat = 'YYYY-MM-DD';
     let date = props.match.params.date;
     let as_date = new Date(date);
 
     let tomorrow = new Date(as_date.getTime() + 24 * 60 * 60 * 1000);
-    tomorrow = tomorrow.getFullYear() + '-' + ('0' + (1 + tomorrow.getMonth())).slice(-2) + '-' + tomorrow.getDate();
+    tomorrow = dateFns.format(tomorrow, dateFormat);
 
     let yesterday = new Date(as_date.getTime() - 24 * 60 * 60 * 1000);
-    yesterday = yesterday.getFullYear() + '-' + ('0' + (1 + yesterday.getMonth())).slice(-2) + '-' + yesterday.getDate();
+    yesterday = dateFns.format(yesterday, dateFormat);
 
-    let next_week = new Date(as_date.getTime() + 7 * 24 * 60 * 60 * 1000);
-    next_week = next_week.getFullYear() + '-' + ('0' + (1 + next_week.getMonth())).slice(-2) + '-' + ('0' + next_week.getDate()).slice(-2);
+    let nextWeek = new Date(as_date.getTime() + 7 * 24 * 60 * 60 * 1000);
+    nextWeek = dateFns.format(nextWeek, dateFormat);
 
-    let last_week = new Date(as_date.getTime() - 7 * 24 * 60 * 60 * 1000);
-    last_week = last_week.getFullYear() + '-' + ('0' + (1 + last_week.getMonth())).slice(-2) + '-' + ('0' + last_week.getDate()).slice(-2);
+    let lastWeek = new Date(as_date.getTime() - 7 * 24 * 60 * 60 * 1000);
+    lastWeek = dateFns.format(lastWeek, dateFormat);
 
     // TODO: doesn't change after going up or back one week until refresh
     return (
         <div className={classes.navigator}>
             <div className={classes.top}>
-                <Link className={classes.button} to={'/daily/' + last_week}> &lt;&lt; </Link>
+                <Link className={classes.button} to={'/daily/' + lastWeek}> &lt;&lt; </Link>
                 <Link className={classes.button} to={'/daily/' + yesterday}> &lt; </Link>
                 <Link className={classes.button} to={'/daily/' + tomorrow}> > </Link>
-                <Link className={classes.button} to={'/daily/' + next_week}>  >> </Link>
+                <Link className={classes.button} to={'/daily/' + nextWeek}>  >> </Link>
             </div>
             <DailyView date={date}/>
         </div>
