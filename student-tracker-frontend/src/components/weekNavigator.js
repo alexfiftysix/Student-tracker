0e6bfe85c69cbad1d1d158a29c0166881dd17fb8
alexfiftysix <amiller5656@gmail.com>
2019-07-15 20:38:40 +1000

    Moved weekNavigator.css into makeStyles

--- student-tracker-frontend/src/components/weekNavigator.js
+++ student-tracker-frontend/src/components/weekNavigator.js
@@ -1,22 +1,35 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import WeeklyView from './WeeklyView'
-import './weekNavigator.css'
 import ChevronLeft from '@material-ui/icons/ChevronLeft'
 import ChevronRight from '@material-ui/icons/ChevronRight'
 import {makeStyles} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     button: {
-        background: 'white',
-        padding: '5px 20px',
+        background: 'none',
+        // padding: '5px 20px',
         textAlign: 'center',
         textDecoration: 'none',
-        margin: '0 5px',
+        margin: '0 2em',
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        color: 'white',
+        '&:visited': {
+            color: 'white',
+        },
+        '&:hover': {
+            textShadow: '0 0 5px rgba(255,255,255,0.4)',
+        }
+    },
+    top: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        margin: '10px',
     }
 }));
 
@@ -33,9 +46,10 @@ export default function WeekNavigator(props) {
 
     return (
         <div className={'week-navigator'}>
-            <div className={'top'}>
+            <div className={classes.top}>
                 <Link className={classes.button} to={'/weekly/' + last_week}><ChevronLeft/><p>Back one week</p></Link>
-                <Link className={classes.button} to={'/weekly/' + next_week}><p>Forwards one week</p><ChevronRight/></Link>
+                <Link className={classes.button} to={'/weekly/' + next_week}><p>Forwards one week</p>
+                    <ChevronRight/></Link>
             </div>
             <WeeklyView start_date={start_date}/>
         </div>
